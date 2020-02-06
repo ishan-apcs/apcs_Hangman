e01b3a4ae6c0541a78ed12787ed982d1c24defa7
Ishan Madan <ishan@mistlogic.com>
2020-02-05 21:57:51 -0800

    added play again

--- Hangman/src/hangman/Hangman.java
+++ Hangman/src/hangman/Hangman.java
@@ -19,65 +19,90 @@ public class Hangman {
     private static Figure figure = new Figure();
     private static Words words = new Words();
     private static Scanner input = new Scanner(System.in);
-    private static int stage = 0;
+    
+    private static int stage;
+    private static String word;
     private static ArrayList<String> usedLetters = new ArrayList<String>();
-    private static String word = words.getRandomWord();
-    private static String[] wordLetters = word.split("");
     private static List<String> resultLetters = new ArrayList<String>();
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        boolean run = true;
 
-        for (int i = 0; i < word.length(); i++) {
-            resultLetters.add("_");
-        }
-        
         System.out.println("Hello there, and welcome to Hangman!");
-        print();
-        
-        while (gameOver() == 0) {
-            System.out.println("Guess a letter (or the word):");
-            String guess = input.nextLine().toLowerCase().replaceAll("[^abcdefghijklmnopqrstuvwxyz]", "");
+
+        while (run) {
+            stage = 0;
+            word = words.getRandomWord();
             
-            while (guess.length() < 1 || usedLetters.indexOf(guess) != -1) {
-                System.out.println("I'm sorry, that's not a valid guess. Please try again:");
-                guess = input.nextLine().toLowerCase().replaceAll("[^abcdefghijklmnopqrstuvwxyz]", "");
+            usedLetters.clear();
+            resultLetters.clear();
+            String[] wordLetters = word.split("");
+
+            for (int i = 0; i < word.length(); i++) {
+                resultLetters.add("_");
             }
 
-            if (word.equals(guess)) {
-                resultLetters = Arrays.asList(guess.split(""));
-            } else if (guess.length() > 1) {
-                System.out.println("\nI'm sorry, that's not the word.");
-                stage++;
-                print();
-            } else {
-                usedLetters.add(guess);
-
-                boolean correct = false;
-                for (int i = 0; i < wordLetters.length; i++) {
-                    if (wordLetters[i].equals(guess)) {
-                        resultLetters.set(i, guess);
-                        correct = true;
-                    }
+            print();
+            
+            while (gameOver() == 0) {
+                System.out.println("Guess a letter (or the word):");
+                String guess = input.nextLine().toLowerCase().replaceAll("[^abcdefghijklmnopqrstuvwxyz]", "");
+                
+                while (guess.length() < 1 || usedLetters.indexOf(guess) != -1) {
+                    System.out.println("I'm sorry, that's not a valid guess. Please try again:");
+                    guess = input.nextLine().toLowerCase().replaceAll("[^abcdefghijklmnopqrstuvwxyz]", "");
                 }
-    
-                if (correct) {
-                    System.out.println("\nThe letter " + guess + " is in the word!");
-                } else {
-                    System.out.println("\nOops! The letter " + guess + " is not in the word!");
+
+                if (word.equals(guess)) {
+                    resultLetters = Arrays.asList(guess.split(""));
+                } else if (guess.length() > 1) {
+                    System.out.println("\nI'm sorry, that's not the word.");
                     stage++;
-                }
-    
-                print();
-            }
-        }
+                    print();
+                } else {
+                    usedLetters.add(guess);
+
+                    boolean correct = false;
+                    for (int i = 0; i < wordLetters.length; i++) {
+                        if (wordLetters[i].equals(guess)) {
+                            resultLetters.set(i, guess);
+                            correct = true;
+                        }
+                    }
         
-        if (gameOver() == 1) {
-            System.out.println("\nCongratulations! You've succeeded at Hangman!");
-        } else if (gameOver() == 2) {
-            System.out.println("\nYou lost! The word was " + word + ".");
+                    if (correct) {
+                        System.out.println("\nThe letter " + guess + " is in the word!");
+                    } else {
+                        System.out.println("\nOops! The letter " + guess + " is not in the word!");
+                        stage++;
+                    }
+        
+                    print();
+                }
+            }
+            
+            if (gameOver() == 1) {
+                System.out.println("\nCongratulations! You've succeeded at Hangman!");
+            } else if (gameOver() == 2) {
+                System.out.println("\nYou lost! The word was " + word + ".");
+            }
+
+            System.out.println("Play again? (yes or no)");
+            String playAgain = input.nextLine().toLowerCase().replaceAll("[^yesno]", "");
+            while (!(playAgain.equals("yes") || playAgain.equals("no"))) {
+                System.out.println("I'm sorry, that's not a valid answer. Please answer yes or no.");
+                playAgain = input.nextLine().toLowerCase().replaceAll("[^yesno]", "");
+            }
+
+            if (playAgain.equals("yes")) {
+                run = true;
+            } else {
+                run = false;
+                System.out.println("Bye!");
+            }
         }
     }
 
